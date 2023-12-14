ddef8b4530fed2d54dffdd558b0acd8f0dd67a67
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-14 09:07:09 +0530

    4A Problem solved , using ternary operator as well as if-else conditional statements , LOGIC :- the number of pieces should be even , if 2%2 ==0 , 2/2 = 1 which is not even , so weight should be either greater than 2 or not equal to 2 , (w%2==0 && w!=2) or (w%2==0 && w>2) ..... or (w%2==0 && w>2) ? YES : NO ; , w!=2 in place of w>2

---
+++ 800RatedProblemsCodeForces/CF_4A_Watermelon.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class CF_4A_Watermelon {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int w = sc.nextInt();
+        if (w%2==0 && w!=2){ // w>2 also can be used 
+            System.out.println("YES");
+        }else{
+            System.out.println("NO");
+        }
+
+        // Alternate method :-
+        System.out.println((w%2==0 && w!=2)? "YES" : "NO");  // w>2 also can be used  
+        sc.close();
+    }
+}
