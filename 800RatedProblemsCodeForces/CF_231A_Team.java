5841079871f54dc13d12e944313cbeb5a9f3d3ef
koushik-dasss <koushikdasmagnate2005@gmail.com>
2023-12-27 09:38:19 +0530

    Logic :- number of questions to be asked in for loop , then every one response in one integer variable , if sum of them is >= 2 , then that question will be selected in total , as a sum

---
+++ 800RatedProblemsCodeForces/CF_231A_Team.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+public class CF_231A_Team {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt(), sum = 0 , ctr = 0;
+        for (int i = 1; i <=n ; i++) {
+            int p = sc.nextInt() , v = sc.nextInt() , t= sc.nextInt() ;
+            sum = p+v+t;
+            if(sum>=2){
+                ctr+=1;
+            }
+
+        }
+        System.out.println(ctr);
+
+
+        sc.close();
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
