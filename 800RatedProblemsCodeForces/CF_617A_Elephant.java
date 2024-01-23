79fe0a017b48cdb60981f79197e728c8a7b984eb
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-23 12:29:43 +0530

    617 A elephant added

--- 800RatedProblemsCodeForces/CF_617A_Elephant.java
+++ 800RatedProblemsCodeForces/CF_617A_Elephant.java
@@ -1,16 +1,15 @@
 import java.util.Scanner;
-
 public class CF_617A_Elephant {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int cf = sc.nextInt() , steps = 0;
-        if (cf%5 == 0){
-            steps = cf/5;
+        int x = sc.nextInt() ;
+        int steps = 0;
+        if (x%5==0){
+            steps = x/5;
         }else{
-            steps = (cf+5)/5; // adding 5 to the co-ordinate point  then dividing by 5 , gets the number of minimal steps
+            steps = (x+5)/5; // precedence of / > + 
         }
         System.out.println(steps);
-        
         sc.close();
     }
 }
