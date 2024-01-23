86257bfd714fcab3a721a312440bdc8710d6e9b4
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-23 12:00:30 +0530

    4A watermelon added

--- 800RatedProblemsCodeForces/CF_4A_Watermelon.java
+++ 800RatedProblemsCodeForces/CF_4A_Watermelon.java
@@ -3,14 +3,19 @@ public class CF_4A_Watermelon {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int w = sc.nextInt();
-        if (w%2==0 && w!=2){ // w>2 also can be used 
+        if (w%2 == 0 && w>2 ){  // w>2 or w!=2 
             System.out.println("YES");
         }else{
             System.out.println("NO");
         }
-
-        // Alternate method :-
-        System.out.println((w%2==0 && w>2)? "YES" : "NO");  
         sc.close();
+
+        /*  Using Ternary Operator (Conditional):-
+        String result = (w%2==0 && w!=2)? "YES" : "NO"; 
+        System.out.println(result);
+
+        */
+
     }
 }
+    
