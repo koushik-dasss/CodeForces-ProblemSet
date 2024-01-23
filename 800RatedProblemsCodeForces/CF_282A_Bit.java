bcecb69bbe45c6a3de39a9d81e9b013fb0f5a9f4
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-23 12:07:20 +0530

    282 A Bit added

--- 800RatedProblemsCodeForces/CF_282A_Bit.java
+++ 800RatedProblemsCodeForces/CF_282A_Bit.java
@@ -2,24 +2,19 @@ import java.util.Scanner;
 public class CF_282A_Bit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int x  = 0 , n = sc.nextInt();
-        for (int i = 1; i <=n ; i++) {
-            String st = sc.next();
+        int x = 0 ;
+        int n = sc.nextInt();
+        for(int i = 1 ; i<= n ; i++){
+            String st =  sc.next();
             if (st.equals("X++")|| st.equals("++X")){
                 x+=1;
             }else if (st.equals("X--")|| st.equals("--X")){
                 x-=1;
-
             }
-
-
-
         }
         System.out.println(x);
-
-
         sc.close();
-    }
+     }
 }
 
 
