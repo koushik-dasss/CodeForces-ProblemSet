033283b0fd39e8f7081442279d97738a35ddf0de
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-23 12:03:39 +0530

    231 A Team added

--- 800RatedProblemsCodeForces/CF_231A_Team.java
+++ 800RatedProblemsCodeForces/CF_231A_Team.java
@@ -2,18 +2,17 @@ import java.util.Scanner;
 public class CF_231A_Team {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt(), sum = 0 , ctr = 0;
-        for (int i = 1; i <=n ; i++) {
-            int p = sc.nextInt() , v = sc.nextInt() , t= sc.nextInt() ;
+        int n = sc.nextInt() , sum = 0 , ctr = 0;
+        for (int i = 1; i <= n; i++) {
+            int p = sc.nextInt();
+            int v = sc.nextInt();
+            int t = sc.nextInt();
             sum = p+v+t;
-            if(sum>=2){
+            if (sum>=2){
                 ctr+=1;
             }
-
         }
         System.out.println(ctr);
-
-
         sc.close();
     }
 }
