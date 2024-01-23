b5995452e17487b0915ab797a77694b128d5aead
koushik-dasss <koushikdasmagnate2005@gmail.com>
2024-01-23 12:11:42 +0530

    50 A Domino added

--- 800RatedProblemsCodeForces/CF_50A_Domino.java
+++ 800RatedProblemsCodeForces/CF_50A_Domino.java
@@ -2,11 +2,11 @@ import java.util.Scanner;
 public class CF_50A_Domino {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int m = sc.nextInt() , n = sc.nextInt();
-        System.out.println((m*n)/2); // No need of int typecasting , as the division will give the result in int data type only
-
-
-
+        int m = sc.nextInt();
+        int n = sc.nextInt();
+        System.out.println(m*n/2);
+        // division in JAVA = int division
+        //  precedence & associativity of * and / are same , no need of (m*n)
         sc.close();
     }
 }
